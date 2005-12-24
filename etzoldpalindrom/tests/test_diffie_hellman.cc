7646e72f765ee2a6d166a163ab0c97e702a9bd01
etzold <etzold>
2005-12-24 14:28:47 +0000

    diffie hellman test

--- etzoldpalindrom/tests/test_diffie_hellman.cc
+++ etzoldpalindrom/tests/test_diffie_hellman.cc
@@ -1,5 +1,6 @@
 #include "diffie_hellman.hh"
 #include <assert.h>
+#include <iostream>
 
 int main( int argc, char** argv ) {
 	char buf[ 4096 ];
@@ -31,4 +32,25 @@ int main( int argc, char** argv ) {
 	gmp_printf( "session key = %Zd\n\n", ska );
 
 	gmp_randclear( rstate );
+
+
+	DES_cblock k1, k2, k3;
+	DES_key_schedule ks1, ks2, ks3;
+	DES_cblock ivec;
+	char in[ 128 ];
+	char out[ 128 ];
+
+	memset( ivec, 0, sizeof( DES_cblock ) );
+	a.get_des3_key( k1, k2, k3 );
+	DES_set_key_unchecked( &k1, &ks1 );
+	DES_set_key_unchecked( &k2, &ks2 );
+	DES_set_key_unchecked( &k3, &ks3 );
+
+	strcpy( in, "12345678" );
+	DES_ede3_cbc_encrypt( (const unsigned char*) in, (unsigned char*) out, strlen( in ), &ks1, &ks2, &ks3, &ivec, DES_ENCRYPT );
+	memset( in, 0, sizeof( in ) );
+	memset( ivec, 0, sizeof( DES_cblock ) );
+	DES_ede3_cbc_encrypt( (const unsigned char*) out, (unsigned char*) in, 8, &ks1, &ks2, &ks3, &ivec, DES_DECRYPT );
+	
+	assert( memcmp( in, "12345678", 8 ) == 0 );
 }
